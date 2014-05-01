95b644f90cfdfc80db5d0eb53a2344bc29d867dc
brambu <brambu@shep.(none)>
2014-05-01 16:07:19 -0700

    adding summer mode to weather

--- bin/weather.py
+++ bin/weather.py
@@ -5,7 +5,17 @@ import pywapi
 import sys
 
 ctof = lambda x: int(((float(x) * 9.0 ) / 5.0 ) + 32.0)
+ftoc = lambda x: int((float(x)  - 32.0) / 1.8 )
 kmhtomph = lambda x: int(float(x) / 1.609344)
+
+def calc_heat_index(t, h):
+    return -42.379 + (2.04901523 * t) +\
+           (10.14333127 * h) - (0.22475541 * t * h) -\
+           (6.83783 * (10**-3) * (t**2)) -\
+           (5.481717 * (10**-2) * (h**2)) +\
+           (1.22874 * (10**-3) * (t**2) * h) +\
+           (8.5282 * (10**-4) * t * (h**2)) -\
+           (1.99 * ( 10**-6) * (t**2) * (h**2))
 
 def weather_for_zip(zipcode):
     try:
@@ -14,7 +24,10 @@ def weather_for_zip(zipcode):
         result = None
     return result
 
-def print_result(result):
+def print_result(result, mode='summer'):
+    #from pprint import pprint as pp
+    #pp(result)
+    #import pdb; pdb.set_trace()
     try:
         title = result['condition']['title']
         text = result['condition']['text']
@@ -24,29 +37,40 @@ def print_result(result):
         forecast = result['forecasts'][0]['text']
         windchillc = result['wind']['chill']
         windspeedkm = result['wind']['speed']
-        printthis = "{0} Currently {1} {2}F/{3}C  "+\
-                    "Forecast: {4} (High {5}F/{6}C - Low {7}F/{8}C) "+\
-                    "Wind: chill {9}F/{10}C speed {11}mph/{12}kmh"
-        printthis = printthis.format(title, text, ctof(tempc), tempc,
-                                     forecast, ctof(high), high, ctof(low), low,
-                                     ctof(windchillc), windchillc, kmhtomph(windspeedkm), int(float(windspeedkm)))
+        humidity = result['atmosphere']['humidity']
+        heatindex = int(calc_heat_index(float(ctof(tempc)), float(humidity)))
+        if mode == 'winter':
+            printthis = "{0} Currently {1} {2}F/{3}C  "+\
+                        "Forecast: {4} (High {5}F/{6}C - Low {7}F/{8}C) "+\
+                        "[Wind: chill {9}F/{10}C speed {11}mph/{12}kmh]"
+            printthis = printthis.format(title, text, ctof(tempc), tempc,
+                                        forecast, ctof(high), high, ctof(low), low,
+                                        ctof(windchillc), windchillc, kmhtomph(windspeedkm), int(float(windspeedkm)))
+        if mode == 'summer':
+            printthis = "{0} Currently {1} {2}F/{3}C "+\
+                        "Forecast: {4} (High {5}F/{6}C - Low {7}F/{8}C) "+\
+                        "[Humidity: {9}% - Heat Index: {10}F/{11}C - Wind: {12}mph/{13}kmh]"
+            printthis = printthis.format(title, text, ctof(tempc), tempc,
+                                        forecast, ctof(high), high, ctof(low), low,
+                                        humidity, heatindex, ftoc(heatindex), kmhtomph(windspeedkm), int(float(windspeedkm)))
     except:
         printthis = 'Not sure.'
-        
+
     print printthis
     return
 
 def main():
-    
+
     parser = ArgumentParser(description='weather fetch by zip')
     parser.add_argument('-z','--zip',required=True,help='zip code')
+    parser.add_argument('-m', '--mode', default='summer', help='summer or winter mode' )
     args = parser.parse_args()
-    
+
     zipcode = args.zip
-    
+    mode = args.mode
     result = weather_for_zip(zipcode)
     if result:
-        print_result(result)
+        print_result(result, mode)
     else:
         print "No results."
 
